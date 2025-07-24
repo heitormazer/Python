db1700d5116aa8b6ded159bae10820a773dd2f9a
HeitorMazer <120420230+heitormazer@users.noreply.github.com>
2025-07-24 14:46:51 -0300

    Add files via upload

---
+++ Aulas/Aula08.py
@@ -0,0 +1,9 @@
+from math import sqrt, floor
+num = int(input('Digite um número: '))
+raiz = sqrt(num)
+print((f'A raiz de {num} é igual a {floor(raiz):.2f}'))
+
+import random
+num = random.randint(1,10)
+print(num)
+
