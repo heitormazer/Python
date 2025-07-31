00fe4dddef27d1906367cd36037e54ae5da82513
HeitorMazer <120420230+heitormazer@users.noreply.github.com>
2025-07-30 22:12:39 -0300

    Add files via upload

---
+++ Exercícios/ex030.py
@@ -0,0 +1,6 @@
+#Crie um programa que leia um número inteiro e mostre na tela se ele é par ou ímpar.
+num = int(input('Digite um numero: '))
+if num % 2 == 0:
+    print('É um número par!')
+else:
+    print('É um número ímpar!')
