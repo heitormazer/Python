e6611099b532bc03a1bad18feca625f902e3abf3
HeitorMazer <120420230+heitormazer@users.noreply.github.com>
2025-07-30 22:03:40 -0300

    Delete Exercícios/Ex23.py

--- Exercícios/Ex23.py
+++
@@ -1,9 +0,0 @@
-num = input("Digite um número entre 0 e 9999: ")
-if not  num.isdigit() or not ( 0 <=int(num) <=9999):
-    print("Número inválido.")
-else:
-    s = num.zfill(4)
-    print(f"Milhar:  {s[0]}")
-    print(f"Centena: {s[1]}")
-    print(f"Dezena:  {s[2]}")
-    print(f"Unidade: {s[3]}")
